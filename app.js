56ce5ebc2037e1860712aeb3411726272cfee259
Abdinasir Mohamed <abdinasir400@gmail.com>
2022-04-01 15:58:59 +0100

    end point users

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const {displayTopics} = require('./controllers/topics.controller.js')
+const {getUsers} = require('./controllers/users.controller.js')
+
 
 app.use(express.json());
 
 app.get('/api/topics',displayTopics)
+app.get('/api/users',getUsers)
 
 app.use((req,res,next) => {
     res.status(404).send({msg: 'path not found'})
